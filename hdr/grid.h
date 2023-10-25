d951d3396e1355b917633c5fa68aaf1302c7933c
Иван Шутёмов <st094830@student.spbu.ru>
2023-10-25 21:39:07 +0300

    macropars head

--- hdr/grid.h
+++ hdr/grid.h
@@ -13,7 +13,7 @@ public:
 	Grid() {};
 	Grid(int x_size, int y_size);
 	~Grid() {};
-	void set_cell_info(int x_coords, int y_coords, double ev);
+	Grid set_cell_info(int x_coords, int y_coords, double ev);
 	double get_cell_info(int x_coords, int y_coords);
 private:
 	//
--- hdr/macropars.h
+++ hdr/macropars.h
@@ -1,1 +1,25 @@
 #pragma once
+#ifndef _MACROPARS_
+#define _MACROPARS_
+
+class Macropars
+{
+	double dens;
+	double pres;
+	double velc;
+	double temp;
+public:
+	Macropars() {};
+	Macropars(double dens, double pres, double velc, double temp);
+	~Macropars() {};
+	void setdens(double dens);
+	void setpres(double pres);
+	void setvelc(double velc);
+	void settemp(double temp);
+	double getdens();
+	double getpres();
+	double getvelc();
+	double gettemp();
+};
+
+#endif
